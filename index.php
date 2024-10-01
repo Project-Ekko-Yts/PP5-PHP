c1f7d0c4ac768be14b258e5ee939e9fb311f82f3
Project-Ekko-Yts <spiritguardudyr890@gmail.com>
2024-10-01 20:42:06 +0800

    PP5-PHP

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+
+<html
+  data-wf-domain="jiancarlos-sublime-site.webflow.io"
+  data-wf-page="66fa170629dfadef4aedcb8f"
+  data-wf-site="66fa170629dfadef4aedcb55"
+  data-wf-status="1"
+>
+  <head>
+    <meta charset="utf-8" />
+    <title>GameDev</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Webflow" name="generator" />
+       <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com" rel="preconnect" />
+    <link
+      href="https://fonts.gstatic.com"
+      rel="preconnect"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+      type="text/javascript"
+    ></script>
+    <script type="text/javascript">
+      WebFont.load({
+        google: {
+          families: [
+            "Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic",
+            "Ubuntu:300,300italic,400,400italic,500,500italic,700,700italic",
+            "Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic",
+            "Roboto:300,regular,500",
+          ],
+        },
+      });
+    </script>
+    <script type="text/javascript">
+      !(function (o, c) {
+        var n = c.documentElement,
+          t = " w-mod-";
+        (n.className += t + "js"),
+          ("ontouchstart" in o ||
+            (o.DocumentTouch && c instanceof DocumentTouch)) &&
+            (n.className += t + "touch");
+      })(window, document);
+    </script>
+    <link
+      href="./assets/logo-footer.png"
+      rel="shortcut icon"
+      type="image/x-icon"
+    />
+    <link
+      href="https://cdn.prod.website-files.com/img/webclip.png"
+      rel="apple-touch-icon"
+    />
+  </head>
+
+  <!-- ####################### HEADER ###################### -->
+
+  <body class="body">
+    <div
+      data-collapse="medium"
+      data-animation="default"
+      data-duration="400"
+      data-easing="ease"
+      data-easing2="ease"
+      role="banner"
+      class="navigation-bar w-nav"
+    >
+      <div class="w-container">
+        <a
+          href=""
+          aria-current="page"
+          class="brand-link w-nav-brand w--current"
+          ><h1 class="brand-text">GameDev</h1></a
+        >
+        <nav role="navigation" class="navigation-menu w-nav-menu">
+          <a
+            href=""
+            aria-current="page"
+            class="navigation-link w-nav-link w--current"
+            >Home</a
+          ><a href="feedback.php" class="navigation-link w-nav-link">Feedbacks</a>
+        </nav>
+        <div class="hamburger-button w-nav-button">
+          <div class="w-icon-nav-menu"></div>
+        </div>
+      </div>
+    </div>
+    <div class="hero-section centered">
+      <div
+        data-w-id="e464d218-f801-55d1-1f50-7da00b5bfb8f"
+        style="opacity: 0"
+        class="w-container"
+      >
+        <h1 data-ix="fade-in-bottom-page-loads" class="hero-heading">
+          gamedev
+        </h1>
+        <div data-ix="fade-in-bottom-page-loads" class="hero-subheading">
+          Discover the Best Free-To-Play Games with Ease!
+        </div>
+        <div data-ix="fade-in-bottom-page-loads">
+          <a href="#Download" class="hollow-button all-caps">let&#x27;s start!</a>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container-2 w-container" >
+        <div class="section-title-group"id="Download">
+          <h2 class="section-heading centered">download for free</h2>
+          <div class="section-subheading center">
+            &quot;Expand your horizons! Dive into new genres and discover
+            exciting adventures beyond your usual games.&quot;
+          </div>
+        </div>
+        <div class="columns w-row">
+          <div class="w-col w-col-4">
+            <div
+              data-w-id="270e8437-efa3-df11-d438-de69b23e41e9"
+              style="opacity: 0"
+              class="white-box"
+            >
+              <img
+                src="https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbde_feather-15-white.svg"
+                alt=""
+                class="grid-image"
+              />
+              <h3>Graphic Design</h3>
+              <p class="paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.
+              </p>
+            </div>
+          </div>
+          <div class="w-col w-col-4">
+            <div
+              data-w-id="29c25774-570b-ddb2-69b5-f4ddbb194afd"
+              style="opacity: 0"
+              class="white-box"
+            >
+              <img
+                src="https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbc7_feather2-17-white.svg"
+                width="210"
+                alt=""
+                class="grid-image"
+              />
+              <h3>Awesome code</h3>
+              <p class="paragraph-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.
+              </p>
+            </div>
+          </div>
+          <div class="w-col w-col-4">
+            <div
+              data-w-id="49e69b8a-ef40-4d84-1f92-d2617143b8db"
+              style="opacity: 0"
+              class="white-box"
+            >
+              <img
+                src="https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbef_feather2-22-white.svg"
+                alt=""
+                class="grid-image"
+              />
+              <h3>free template</h3>
+              <p class="paragraph-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section accent">
+      <div class="w-container">
+        <div class="section-title-group">
+          <h2 class="section-heading centered white">most popular games</h2>
+          <div class="section-subheading center off-white">
+            This is some text inside of a div block.
+          </div>
+        </div>
+        <div class="w-row">
+          <div class="w-col w-col-6">
+            <div class="white-box transparent">
+              <img
+                src="https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbfd_city-scape.jpg"
+                sizes="(max-width: 479px) 85vw, (max-width: 767px) 88vw, (max-width: 991px) 324px, 430px"
+                srcset="
+                  https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbfd_city-scape-p-500x334.jpeg   500w,
+                  https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbfd_city-scape-p-1080x721.jpeg 1080w,
+                  https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbfd_city-scape.jpg             1300w
+                "
+                alt=""
+                class="fullwidth-image"
+              />
+              <h3 class="white-text">SERVICES one</h3>
+              <p class="white-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Suspendisse varius.
+              </p>
+              <a href="#" class="hollow-button">Learn more</a>
+            </div>
+          </div>
+          <div class="w-col w-col-6">
+            <div class="white-box transparent">
+              <img
+                src="https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbfb_photo-1416400639808-f41f0c149b09.jpg"
+                sizes="(max-width: 479px) 85vw, (max-width: 767px) 88vw, (max-width: 991px) 324px, 430px"
+                srcset="
+                  https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbfb_photo-1416400639808-f41f0c149b09-p-1080x721.jpeg 1080w,
+                  https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbfb_photo-1416400639808-f41f0c149b09.jpg             1300w
+                "
+                alt=""
+                class="fullwidth-image"
+              />
+              <h3 class="white-text">SERVICES two</h3>
+              <p class="white-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Suspendisse varius.
+              </p>
+              <a href="#" class="hollow-button all-caps">Learn more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div
+        data-delay="4000"
+        data-animation="slide"
+        class="w-slider"
+        data-autoplay="false"
+        data-easing="ease"
+        data-hide-arrows="false"
+        data-disable-swipe="false"
+        data-autoplay-limit="0"
+        data-nav-spacing="3"
+        data-duration="500"
+        data-infinite="true"
+      >
+        <div class="w-slider-mask">
+          <div class="slide w-slide"></div>
+          <div class="slide-2 w-slide"></div>
+        </div>
+        <div class="w-slider-arrow-left">
+          <div class="w-icon-slider-left"></div>
+        </div>
+        <div class="w-slider-arrow-right">
+          <div class="w-icon-slider-right"></div>
+        </div>
+        <div class="slide-nav w-slider-nav w-round w-num"></div>
+      </div>
+      <div class="container w-container">
+        <div class="section-title-group">
+          <h2 class="section-heading centered">Send us your feedback</h2>
+          <div class="section-subheading center">
+            &quot;Your feedback helps us level up! We appreciate your insights
+            as we work to improve your gaming experience.&quot;
+          </div>
+        </div>
+        <div
+          data-duration-in="300"
+          data-duration-out="100"
+          data-current="Tab 3"
+          data-easing="ease"
+          class="tabs-wrapper w-tabs"
+        >
+          <div class="w-tab-content">
+            <div data-w-tab="Tab 1" class="w-tab-pane">
+              <div>
+                <img
+                  src="https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbff_photo-1422222948315-28aadb7a2cb8.jpg"
+                  sizes="(max-width: 767px) 96vw, (max-width: 991px) 728px, 940px"
+                  srcset="
+                    https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbff_photo-1422222948315-28aadb7a2cb8-p-1080x721.jpeg 1080w,
+                    https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbff_photo-1422222948315-28aadb7a2cb8.jpg             1300w
+                  "
+                  alt=""
+                  class="fullwidth-image"
+                />
+                <h2>Some Title Here</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in eros elementum tristique.
+                  <br />Duis cursus, mi quis viverra ornare, eros dolor interdum
+                  nulla, ut commodo diam libero vitae erat. <br />Aenean
+                  faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc
+                  ut sem vitae risus tristique posuere.
+                </p>
+              </div>
+            </div>
+            <div data-w-tab="Tab 2" class="w-tab-pane">
+              <div>
+                <img
+                  src="https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbea_5fcb0a55.jpg"
+                  sizes="(max-width: 767px) 96vw, (max-width: 991px) 728px, 940px"
+                  srcset="
+                    https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbea_5fcb0a55-p-500x334.jpeg  500w,
+                    https://cdn.prod.website-files.com/66fa170629dfadef4aedcb55/66fa170629dfadef4aedcbea_5fcb0a55.jpg            1300w
+                  "
+                  alt=""
+                  class="fullwidth-image"
+                />
+                <h2>Heading</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in eros elementum tristique.
+                  <br />Duis cursus, mi quis viverra ornare, eros dolor interdum
+                  nulla, ut commodo diam libero vitae erat.<br />Aenean faucibus
+                  nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem
+                  vitae risus tristique posuere.
+                </p>
+              </div>
+            </div>
+            <div data-w-tab="Tab 3" class="w-tab-pane w--tab-active">
+              <div class="w-form">
+                <form
+                  id="email-form"
+                  name="email-form"
+                  data-name="Email Form"
+                  method="get"
+                  class="form-2"
+                  data-wf-page-id="66fa170629dfadef4aedcb8f"
+                  data-wf-element-id="7dc4bbe6-b4b6-3c74-795d-fb2832fe6c2a"
+                >
+                  <label for="name" class="field-label-3">Name</label
+                  ><input
+                    class="text-field-3 w-input"
+                    maxlength="256"
+                    name="name"
+                    data-name="Name"
+                    placeholder="Enter your name"
+                    type="text"
+                    id="name"
+                  /><label for="Email-3" class="field-label-2">Email</label
+                  ><input
+                    class="text-field-2 w-input"
+                    maxlength="256"
+                    name="Email"
+                    data-name="Email"
+                    placeholder="Enter your email"
+                    type="email"
+                    id="Email-3"
+                  /><label for="email" class="field-label">Feedback</label
+                  ><input
+                    class="text-field w-input"
+                    maxlength="256"
+                    name="email"
+                    data-name="Email"
+                    placeholder="Enter your message"
+                    type="text"
+                    id="email"
+                  /><input
+                    type="submit"
+                    data-wait="Please wait..."
+                    class="w-button"
+                    value="Submit"
+                  />
+                </form>
+                <div class="w-form-done">
+                  <div>Thank you! Your submission has been received!</div>
+                </div>
+                <div class="w-form-fail">
+                  <div>
+                    Oops! Something went wrong while submitting the form.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include './components/footer.php' ?>
+    <script
+      src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66fa170629dfadef4aedcb55"
+      type="text/javascript"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+      crossorigin="anonymous"
+    ></script>
+    <script src="./js/animation.js" type="text/javascript"></script>
+  </body>
+</html>
